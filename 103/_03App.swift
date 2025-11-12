42243541d96c72573f19445701368d658180ded4
RHayes1302 <ramone.hayes@yahoo.com>
2025-11-12 13:43:57 -0800

    Initial Commit

---
+++ 103/_03App.swift
@@ -0,0 +1,17 @@
+//
+//  _03App.swift
+//  103
+//
+//  Created by Ramone Hayes on 11/12/25.
+//
+
+import SwiftUI
+
+@main
+struct _03App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ 103Tests/_03Tests.swift
@@ -0,0 +1,16 @@
+//
+//  _03Tests.swift
+//  103Tests
+//
+//  Created by Ramone Hayes on 11/12/25.
+//
+
+import Testing
+
+struct _03Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
